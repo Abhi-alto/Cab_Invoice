114ee9371225f2b25d2a6426af030ed44606cac7
unknown <abhisheksrivastava9719@gmail.com>
2022-07-21 22:41:28 +0530

    [Abhishek] Added Premium service functionality

--- CabInvoice/CabRide.cs
+++ CabInvoice/CabRide.cs
@@ -11,31 +11,44 @@ namespace Invoice_Generator
         public int Tride = 0;
         public double avg = 0;
         public double TotalFare = 0;
+  
         public moreDetails(int totRides, double Tfare)
         {
             this.Tride = totRides;
             this.TotalFare = Tfare;
             avg = TotalFare / Tride;
+
         }
 
     }
     public class ride
     {
+        public bool premium = false;
         public double km = 0,
                hrs = 0;
-        public ride(double km, double hrs)
+        public ride(double km, double hrs, bool premium = false)
         {
             this.km = km;
             this.hrs = hrs;
+            this.premium = premium;
         }
     }
     public class CabRide
     {
-        const double Rate_PKM = 10,
+                    double Rate_PKM = 10,
                      Rate_pHr = 1,
-                     min_fare = 5;
-        public double Ride(double km, double hrs)
+                     min_fare = 5,
+                     PremiumRate_PerKm=15,
+                     Pre_Rate_pHr = 2,
+                     Pre_min_fare = 20;
+        public double Ride(double km, double hrs, bool premium = false)
         {
+            if(premium)
+            {
+                Rate_PKM = PremiumRate_PerKm;
+                Rate_pHr = Pre_Rate_pHr;
+                min_fare = Pre_min_fare;
+            }
             if ((km * Rate_PKM + hrs * Rate_pHr) >= min_fare)
             {
                 return (km * Rate_PKM + hrs * Rate_pHr);
@@ -50,7 +63,7 @@ namespace Invoice_Generator
             double total = 0;
             foreach (var cost in multipleRides)
             {
-                total += Ride(cost.km, cost.hrs);
+                total += Ride(cost.km, cost.hrs,cost.premium);
             }
             moreDetails obj = new moreDetails(multipleRides.Count, total);                  //passed to calculate avg cost/ride
             return obj;
---
+++ TestProject1/IsPremium.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Invoice_Generator;
+using CabInvoice;
+
+namespace TestProject1
+{
+    [TestClass]
+    internal class IsPremium
+    {
+        [TestMethod]
+        public void TestPremium()
+        {
+            CabRide cab = new CabRide();
+            List<ride> faresSum = new List<ride>()
+            {
+                new ride(0.3, 1,true),
+                new ride(10, 1),
+                new ride(20, 3,true ),
+                new ride(40, 2),
+                new ride(15, 4,true)
+            };
+            moreDetails result = cab.ManyRidesSum(faresSum);
+            Assert.AreEqual(1225, result.TotalFare);
+        }
+    }
+}
