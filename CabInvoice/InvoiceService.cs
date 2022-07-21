36e40c6259a0e156e2f48bc478fa7cbc7d3318f7
unknown <abhisheksrivastava9719@gmail.com>
2022-07-21 17:58:45 +0530

    [Abhishek] Added Invoice Services functionality

---
+++ CabInvoice/InvoiceService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Invoice_Generator;
+
+namespace CabInvoice
+{
+    public class InvoiceService
+    {
+        /// <InvoiceServices>
+        /// used for storing the Dictionary having ID as key and List of othe details as the value>
+        /// <AddToList>
+        /// used for adding a list of rides in the DataBase dictionary
+        /// <moreDetails>
+        /// used for geting the details of a particular ID and returning the vtotalFare of that particular Id with all its rides
+        Dictionary<int,List<ride>> DataBase = new Dictionary<int,List<ride>>();
+        public moreDetails getFare(int UserID)
+        {
+            CabRide cab = new CabRide();
+            List<ride> list = DataBase[UserID];
+            moreDetails result = new moreDetails(0, 0);
+            if (list != null && list.Count() > 0)
+            {
+                result = cab.ManyRidesSum(DataBase[UserID]);
+            }
+            return result;
+        }
+        public void AddToList(int UserID,List <ride> RidesList)
+        {
+            List<ride> list = new List<ride>();
+            var res = DataBase[UserID];
+            if(res!=null)
+            {
+                list=res;
+            }
+            foreach(var rides in RidesList)
+            {
+                list.Add(rides);
+            }
+            DataBase[UserID] = list;
+        }
+    }
+}
---
+++ TestProject1/InvoiceService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Invoice_Generator;
+using CabInvoice;
+
+namespace TestProject1
+{
+    [TestClass]
+    internal class InvoiceServiceTest
+    {
+        [TestMethod]
+        public void Service()
+        {
+            int Id = 2;
+            InvoiceService user = new InvoiceService();
+            user.AddToList(2, new List<ride>()
+            {
+                new ride(0.3, 1),
+                new ride(10, 1),
+                new ride(20, 3 ),
+                new ride(40, 2),
+                new ride(15, 4)
+            });
+            user.AddToList(3, new List<ride>()
+            {
+                new ride(10, 1),
+                new ride(20, 3 ),
+                new ride(40, 2),
+                new ride(15, 4)        //861
+            });
+            var res = user.getFare(2);
+            Assert.AreEqual(865, res.TotalFare);
+        }
+    }
+}
